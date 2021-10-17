e8266624e349639064a68e9fa555dfab78efeb70
Aiswarya-Raju <83088089+Aiswarya-Raju@users.noreply.github.com>
2021-10-17 14:54:25 +0530

    settings change

--- register_login/settings.py
+++ register_login/settings.py
@@ -120,4 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [STATIC_DIR,]
+#STATICFILES_DIRS = [STATIC_DIR,]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
