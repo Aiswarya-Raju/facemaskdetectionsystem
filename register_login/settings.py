e2754f97370a43f4652a102b38f102bd39d935e6
Aiswarya-Raju <83088089+Aiswarya-Raju@users.noreply.github.com>
2021-10-17 16:35:26 +0530

    settings

--- register_login/settings.py
+++ register_login/settings.py
@@ -118,7 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+BASE_DIR=os.path.dirname(os.path.dirname(__file__))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 #STATICFILES_DIRS = [STATIC_DIR,]
 STATICFILES_DIRS = (
